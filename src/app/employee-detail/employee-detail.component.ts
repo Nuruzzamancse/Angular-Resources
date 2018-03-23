1a1bd1e52af04b22612d70293a003c7876f4d725
Nuruzzamancse <mr.nuruzzamancse@gmail.com>
2018-03-23 18:42:19 +0600

    employee list and details

---
+++ src/app/employee-detail/employee-detail.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import {EmployeeService} from '../employee.service';
+
+@Component({
+  selector: 'app-employee-detail',
+  templateUrl: './employee-detail.component.html',
+  styleUrls: ['./employee-detail.component.css']
+})
+export class EmployeeDetailComponent implements OnInit {
+
+  public employees = [];
+  public errorMsg;
+
+  constructor(private _employeeService: EmployeeService ) { }
+
+  // Service
+  // ngOnInit() {
+  //   this.employees = this._employeeService.getEmployees();
+  // }
+
+  // subscribe
+
+  // ngOnInit () {
+  //   this._employeeService.getEmployees()
+  //     .subscribe( data => this.employees = data);
+  // }
+  // error handling
+  ngOnInit () {
+    this._employeeService.getEmployees()
+      .subscribe( data => this.employees = data,
+        error => this.errorMsg = error);
+  }
+
+}
